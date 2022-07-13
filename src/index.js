33af77c768dea67701aca0b82b99c75c007bcef7
Elena <lenyxa2004@gmail.com>
2022-07-13 22:40:04 +0300

    Last updated

--- src/index.js
+++ src/index.js
@@ -51,6 +51,26 @@ function printDate(date) {
   document.getElementById("result_min").innerHTML = min;
 }
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  let min = date.getMinutes();
+  if (min < 10) {
+    min = "0" + min;
+  }
+
+  let day = date.getDate();
+  document.getElementById("last").innerHTML = "Last updated: ";
+  document.getElementById("result_weekDay").innerHTML = getWeekDay(date) + ", ";
+  document.getElementById("result_month").innerHTML = getMonth(date);
+  document.getElementById("result_day").innerHTML = day;
+  document.getElementById("result_hours").innerHTML = hours + ":";
+  document.getElementById("result_min").innerHTML = min;
+}
+
 //var temp; // обьявили глобальную переменную чтобы ее было видно из функции
 
 function searchCity(city) {
@@ -73,6 +93,8 @@ function searchCity(city) {
     let clouds = res.data.weather[0].main;
     console.log(clouds);
     imgClouds(clouds);
+    console.log(res.data.dt * 1000);
+    formatDate(res.data.dt * 1000);
   });
 }
 
@@ -99,6 +121,9 @@ function imgClouds(clouds1) {
   if (clouds1 === "Haze") {
     imgC111.src = imgs[5];
   }
+  if (clouds1 === "Mist") {
+    imgC111.src = imgs[5];
+  }
 }
 
 function search(event) {
@@ -111,6 +136,7 @@ function search(event) {
 }
 
 let apiKey = "50e56fa212f8363db506fc2abece70d9";
+
 function searchLocation(position) {
   let apiKey = "50e56fa212f8363db506fc2abece70d9";
 
@@ -133,8 +159,83 @@ function searchLocation(position) {
     document.getElementById("result_city").innerHTML = res.data.name;
     let clouds = res.data.weather[0].main;
     imgClouds(clouds);
+    formatDate(res.data.dt * 1000);
   });
 }
+//*************************** *
+
+function getPrognoz() {
+  //event.preventDefault();
+  navigator.geolocation.getCurrentPosition(Prognoz);
+}
+
+function Prognoz(position) {
+  let apiKeyP = "50e56fa212f8363db506fc2abece70d9";
+  let latP = position.coords.latitude;
+  let lonP = position.coords.longitude;
+  let apiUrlP = `http://api.openweathermap.org/data/2.5/forecast/?lat=${latP}&lon=${lonP}&units=metric&count=10&appid=${apiKeyP}`;
+  //let apiUrlPP = `http://api.openweathermap.org/data/2.5/weather?lat=${latP}&lon=${lonP}&lang=en&units=metric&appid=${apiKey}`;
+
+  axios.get(apiUrlP).then((res) => {
+    console.log(res);
+    console.log(res.data.list[1].main.temp);
+    console.log(res.data.list[1].wind.speed);
+    console.log(res.data.list[1].weather[0].main);
+    // эти данне надо пораспихивать в прогнозы выюрать с каким индексом будет подходящий день и час
+    //
+    /*let h1 = document.querySelector("h1");
+   h1.innerHTML = Math.round(res.data.main.temp) + "°C";
+   document.getElementById("result_humidity").innerHTML =
+     res.data.main.humidity;
+   document.getElementById("result_wind").innerHTML =
+     "Wind: " + res.data.wind.speed + "km/h";
+   document.getElementById("result_clouds").innerHTML =
+     res.data.weather[0].main;
+   document.getElementById("result_city").innerHTML = res.data.name;
+   let clouds = res.data.weather[0].main;
+   imgClouds(clouds);*/
+  });
+}
+//******************************************************************** */
+//******************************************************************** */
+
+//*************************** *
+
+function getHistory() {
+  //event.preventDefault();
+  navigator.geolocation.getCurrentPosition(HistoryP);
+}
+
+function HistoryP(position) {
+  let apiKeyP = "50e56fa212f8363db506fc2abece70d9";
+  let latP = position.coords.latitude;
+  let lonP = position.coords.longitude;
+  let dtP = 1586468027;
+  //let apiUrlP = `http://api.openweathermap.org/data/2.5/forecast/?lat=${latP}&lon=${lonP}&units=metric&count=10&appid=${apiKeyP}`;
+  //let apiUrlPP = `http://api.openweathermap.org/data/2.5/weather?lat=${latP}&lon=${lonP}&lang=en&units=metric&appid=${apiKey}`;
+  let apiUrlH = `https://api.openweathermap.org/data/2.5/timemachine?lat=${latP}&lon=${lonP}&dt=${dtP}&appid=${apiKeyP}`;
+  axios.get(apiUrlH).then((res) => {
+    console.log(res);
+    console.log(res);
+    console.log(res);
+    console.log(res);
+    // эти данне надо пораспихивать в прогнозы выюрать с каким индексом будет подходящий день и час
+    //
+    /*let h1 = document.querySelector("h1");
+   h1.innerHTML = Math.round(res.data.main.temp) + "°C";
+   document.getElementById("result_humidity").innerHTML =
+     res.data.main.humidity;
+   document.getElementById("result_wind").innerHTML =
+     "Wind: " + res.data.wind.speed + "km/h";
+   document.getElementById("result_clouds").innerHTML =
+     res.data.weather[0].main;
+   document.getElementById("result_city").innerHTML = res.data.name;
+   let clouds = res.data.weather[0].main;
+   imgClouds(clouds);*/
+  });
+}
+//******************************************************************** */
+//******************************************************************** */
 
 function displayWeatherCondition(response) {
   console.log(res.data.main.temp);
@@ -160,3 +261,12 @@ searchForm.addEventListener("submit", search);
 
 var currentLocationButton = document.querySelector("#current-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+console.log("*******************************");
+console.log("*******************************");
+var currentLocationButtonP = document.querySelector("#current-buttonP");
+currentLocationButtonP.addEventListener("click", getPrognoz);
+console.log("*******************************");
+var currentLocationButtonP = document.querySelector("#current-buttonH");
+currentLocationButtonP.addEventListener("click", getHistory);
+console.log("*******************************");
